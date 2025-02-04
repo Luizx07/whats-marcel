d463e32ddd0e93ceca66658b6d977495cc197164
LUIZ HENRIQUE DOS SANTOS <24122470@CFP127ALUNO.LOCAL>
2025-02-04 14:38:37 -0300

    ok 2, 3 e 4 ok

--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -42,4 +42,52 @@ const getDadosPerfil = function(user){
     return usuarios
     }
 
-console.log(getListaDeDadosPessoais(1))
+//console.log(getListaDeDadosPessoais(1))
+
+const dadosDeContatos = function(numero){
+    let user =  String(numero)
+    let dados = []
+    let usuarios = {}
+
+    modulo_contato.contatos.whatsUsers.forEach(function(item){
+        item.contacts.forEach (function(item2){
+            if(String(item.number) == user)
+                dados.push({
+                name : item2.name,
+                imagem : item2["image"],
+                descricao : item2.description
+                })
+                
+            })
+            
+            
+    })
+
+    usuarios.dados_pessoais = dados
+    
+    return usuarios
+    }
+// console.log(dadosDeContatos('11955577796'))
+
+const getConversas = function(numero){
+    let user = String(numero)
+    let usuarios = {}
+    let dados = []
+
+    modulo_contato.contatos.whatsUsers.forEach(function(item){
+        item.contacts.forEach(function(item2){
+            if(String(item.number) == user){
+                dados.push({
+                    name: item2.name,
+                    profile: item2["image"],
+                    description: item2.description,
+                    convensas: item2.messages
+                })
+            }
+        })
+    })
+    usuarios.conversas = dados
+
+    return usuarios
+}
+console.log(getConversas('11955577796'))
