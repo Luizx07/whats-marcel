ff66c894520922e06edfa295cdb93f4a8e717cfe
LUIZ HENRIQUE DOS SANTOS <24122470@CFP127ALUNO.LOCAL>
2025-02-04 16:56:26 -0300

    ok 2, 3, 4, 5 e 6 ok

--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -81,7 +81,7 @@ const getConversas = function(numero){
                     name: item2.name,
                     profile: item2["image"],
                     description: item2.description,
-                    convensas: item2.messages
+                    conversas: item2.messages
                 })
             }
         })
@@ -90,4 +90,68 @@ const getConversas = function(numero){
 
     return usuarios
 }
-console.log(getConversas('11955577796'))
+// console.log(getConversas('11955577796'))
+
+const getConversasPorNome = function(numero, conta){
+    let status = false
+    let numb = String(numero)
+    let nome = String(conta).toUpperCase()
+    let usuarios = {}
+    let dados = []
+
+    modulo_contato.contatos.whatsUsers.forEach(function(item){
+        item.contacts.forEach(function(item2){
+
+            if(String(item.number) == numb && String(item2.name).toUpperCase() == nome){
+
+ 
+                dados.push({
+                    name: item2.name,
+                    profile: item2["image"],
+                    description: item2.description,
+                    conversas: item2.messages
+                })
+
+                status = true
+                
+            }
+        })
+    })
+    usuarios.conversas = dados
+
+    if(status == true){
+        return usuarios
+    }else{
+        return status
+    }
+}
+// console.log(getConversasPorNome("11987876567", "Jane Smith"))
+
+const getPalavra = function(numero, palavra, nome){
+    let nick = String(nome).toUpperCase()
+    let numb = String(numero)
+    let filtro = String(palavra).toUpperCase()
+    let conversa = {}
+    let dados = []
+
+    modulo_contato.contatos.whatsUsers.forEach(function(item){
+        item.contacts.forEach(function(item2){
+            item2.messages.forEach(function(item3){
+                if(String(item.number) == numb && String(item3.content).toUpperCase().includes(filtro)){
+                    dados.push({
+                        nome: item2.name,
+                        perfil: item2.image,
+                        descricao: item2.description,
+                        conversas: item2.messages
+                    })
+                }
+            })
+        })
+    })
+    conversa.conversas = dados
+    
+    return conversa
+
+ }
+
+ console.log(getPalavra("11987876567", "Oi, como está indo o projeto?"))
